0cb08e49b1d065782a5140998362617cd1687aa9
Egor <egorkaraulov22@gmail.com>
2021-09-12 23:41:05 +0400

    Адаптивная графика всех страниц

--- source/js/index.js
+++ source/js/index.js
@@ -1,82 +1,100 @@
 const switcher = document.querySelector(".switcher");
-const switcherWrapper = switcher.querySelector(".switcher__wrapper");
-const switcherRadios = switcherWrapper.querySelectorAll(".switcher__radio");
+if (switcher) {
+  const switcherWrapper = switcher.querySelector(".switcher__wrapper");
+  const switcherRadios = switcherWrapper.querySelectorAll(".switcher__radio");
 
-switcherWrapper.addEventListener("focusin", (evt) => {
-  switcherWrapper.addEventListener("keydown", (evt) => {
-    if (event.key === "Enter" || event.key === " ") {
-      evt.preventDefault();
-      toogleSwitch();
-    }
+  switcherWrapper.addEventListener("focusin", (evt) => {
+    switcherWrapper.addEventListener("keydown", (evt) => {
+      if (event.key === "Enter" || event.key === " ") {
+        evt.preventDefault();
+        toogleSwitch();
+      }
+    });
   });
-});
 
-function toogleSwitch() {
-  if (switcherRadios[0].checked) {
-    switcherRadios[1].checked = true;
-  } else {
-    switcherRadios[0].checked = true;
+  function toogleSwitch() {
+    if (switcherRadios[0].checked) {
+      switcherRadios[1].checked = true;
+    } else {
+      switcherRadios[0].checked = true;
+    }
   }
 }
 
-/* comparison.js */
+  /* comparison.js */
 
 const compasion = document.querySelector(".comparison");
-const compasionWrapper = compasion.querySelector(".comparison__slides");
-const WIDTH = compasionWrapper.offsetWidth;
-const compasionSlides = compasionWrapper.querySelectorAll(".comparison__slide");
-const compasionRange = compasion.querySelector(".comparison__range");
 
-const compasionSwitcher = compasion.querySelector(".comparison__switcher");
-const compasionRadios = compasionSwitcher.querySelectorAll(".switcher__radio");
-const compasionControls = compasion.querySelectorAll("a.comparison__control");
+if (compasion) {
 
-compasionSwitcher.hidden = false;
-compasionRange.hidden = false;
-compasionRange.parentElement.classList.add(
-  `${compasionRange.parentElement.classList[0]}--activated`
-);
+  const compasionWrapper = compasion.querySelector(".comparison__slides");
+  const WIDTH = compasionWrapper.offsetWidth;
+  const compasionSlides = compasionWrapper.querySelectorAll(".comparison__slide");
+  const compasionRange = compasion.querySelector(".comparison__range");
 
-compasionSwitcher.addEventListener("change", (evt) => {
-  const scrollPosition = evt.target.id.includes("after") ? WIDTH : 0;
+  const compasionSwitcher = compasion.querySelector(".comparison__switcher");
+  const compasionRadios = compasionSwitcher.querySelectorAll(".switcher__radio");
+  const compasionControls = compasion.querySelectorAll("a.comparison__control");
 
-  compasionWrapper.scrollTo({
-    left: scrollPosition,
-    behavior: "smooth"
-  });
-});
+  compasionSwitcher.hidden = false;
+  compasionRange.hidden = false;
+  compasionRange.parentElement.classList.add(
+    `${compasionRange.parentElement.classList[0]}--activated`
+    );
 
-compasionRange.addEventListener("input", updateSlider);
+    compasionSwitcher.addEventListener("change", (evt) => {
+      const scrollPosition = evt.target.id.includes("after") ? WIDTH : 0;
 
-compasionControls.forEach((control, index) => {
-  control.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    compasionRange.value =
-      index === 0 ? compasionRange.min : compasionRange.max;
-    updateSlider();
-  });
-});
+      compasionWrapper.scrollTo({
+        left: scrollPosition,
+        behavior: "smooth"
+      });
+    });
 
-function updateSlider() {
-  const inverseValue = compasionRange.max - compasionRange.value;
+    compasionRange.addEventListener("input", updateSlider);
 
-  compasionWrapper.style.setProperty("--slider-divide", inverseValue + "%");
+    compasionControls.forEach((control, index) => {
+      control.addEventListener("click", (evt) => {
+        evt.preventDefault();
+        compasionRange.value =
+        index === 0 ? compasionRange.min : compasionRange.max;
+        updateSlider();
+      });
+    });
 
-  const parentWidth = compasion.parentNode.offsetWidth;
-  let backgroundDividePoint =
-    compasionWrapper.offsetLeft + (WIDTH * inverseValue) / compasionRange.max;
+    function updateSlider() {
+      const inverseValue = compasionRange.max - compasionRange.value;
 
-  updateBackround(backgroundDividePoint + "px");
+      compasionWrapper.style.setProperty("--slider-divide", inverseValue + "%");
 
-  if (compasionRange.value === compasionRange.max) {
-    updateBackround(0);
+      const parentWidth = compasion.parentNode.offsetWidth;
+      let backgroundDividePoint =
+      compasionWrapper.offsetLeft + (WIDTH * inverseValue) / compasionRange.max;
+
+      updateBackround(backgroundDividePoint + "px");
+
+      if (compasionRange.value === compasionRange.max) {
+        updateBackround(0);
+      }
+
+      if (compasionRange.value === compasionRange.min) {
+        updateBackround("100%");
+      }
+    }
+
+    function updateBackround(value) {
+      compasion.parentNode.style.setProperty("--back-divide", value);
+    }
   }
 
-  if (compasionRange.value === compasionRange.min) {
-    updateBackround("100%");
-  }
-}
 
-function updateBackround(value) {
-  compasion.parentNode.style.setProperty("--back-divide", value);
-}
+// hamburger
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector('.main-nav__toggle');
+  const nav = document.querySelector('.main-nav');
+
+  btn.addEventListener('click', () => {
+    nav.classList.toggle('main-nav--closed');
+    nav.classList.toggle('main-nav--opened');
+  });
+});
